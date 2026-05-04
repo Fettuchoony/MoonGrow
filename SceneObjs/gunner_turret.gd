77d07c65aae70ba117e16dad373694e2e2b5be5b
unknown <wery231@gmail.com>
2026-05-03 22:18:26 -0700

    Midway through adding turret functionality

--- SceneObjs/gunner_turret.gd
+++ SceneObjs/gunner_turret.gd
@@ -1,5 +1,7 @@
 extends RigidBody3D
 
+@onready var enemy_detection = $EnemyDetect
+
 var being_held
 var hold_pos
 
@@ -14,6 +16,7 @@ func _process(delta: float) -> void:
 	pass
 
 func _physics_process(delta: float) -> void:
+	if enemy_detection.has_overlapping_bodies(): print("enemy detected")
 	if being_held:
 		gravity_scale = 0
 		linear_velocity = global_position.distance_to(hold_pos.global_position) * global_position.direction_to(hold_pos.global_position)
