6e7912b09db9c91ccf5662db79963faf6850a225
Fettuchoony <wery231@gmail.com>
2026-08-11 15:52:29 -0700

    Pickup physics working, todo: add pickup to player

--- SceneObjs/Entity/pickup.gd
+++ SceneObjs/Entity/pickup.gd
@@ -1,6 +1,8 @@
 class_name Pickup extends Sprite3D
 
 const GRAV : float = -9.8
+const AMPLITUDE : float = 5.0
+const FREQ : float = 2.0
 
 var dmg_mod : Item = preload("res://SceneObjs/Modifiers/dmg_mod_tier1.tscn").instantiate()
 
@@ -13,7 +15,9 @@ var _vel : Vector3
 var _pickup_area : Area3D
 # if object is landed, item no longer scans for collision, is only capturable
 var _landed : bool
+var _time : float = 0
 
+# TODO: Change this to a custom default item, not just the dmg modifier
 func _init(item : Item = dmg_mod) -> void:
 	_item = item 
 
@@ -27,8 +31,18 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	_fall_and_pickup(delta)
+	
+func _fall_and_pickup(delta: float) -> void:
+	# Falling and rotation
 	if !_landed && _pickup_area.get_overlapping_areas().size() == 0 && _pickup_area.get_overlapping_bodies().size() == 0:
 		_vel.y += GRAV * delta
 		global_position += delta * _vel
 	else:
 		_landed = true
+		offset.y = AMPLITUDE * -cos(FREQ * _time) + ((pixel_size / 0.01) * AMPLITUDE)
+		rotation.y += delta
+	
+	# IMPORTANT: Player pickup is handled in player movement script
+	
+	_time += delta
--- SceneObjs/Entity/pickup_emmiter.gd
+++ SceneObjs/Entity/pickup_emmiter.gd
@@ -15,6 +15,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if time > frequency:
-		target.new()
+		pass
+		target.new(preload("res://SceneObjs/Modifiers/dmg_mod_tier1.tscn").instantiate())
 		
 	time += delta
