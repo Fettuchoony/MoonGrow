727e60d923c763b4b9ab19264d3ffa786f390903
Fettuchoony <wery231@gmail.com>
2026-08-25 13:26:48 -0700

    Fixing up the portals, almost there

--- SceneObjs/Environmental/portal.gd
+++ SceneObjs/Environmental/portal.gd
@@ -1,18 +1,17 @@
 extends MeshInstance3D
 
-@onready var _overworld : Node3D = $"../CurrentLevel/MainTestScene"
-@onready var _underworld : Node3D = $"../CurrentLevel/Underworld"
+@onready var _main_viewport_cam_parent = $"../MainPlayer/CameraPivot/SpringArm3D"
+@onready var _portal_viewport_cam_parent = $"../MainPlayer/PortalViewport"
 @onready var _player_overworld_cam : Camera3D = $"../MainPlayer/CameraPivot/SpringArm3D/Camera3D"
 @onready var _player_underworld_cam : Camera3D = $"../MainPlayer/PortalViewport/UnderworldCam"
 #@onready var _lighting : DirectionalLight3D = $"../DirectionalLight3D"
 @onready var _remote_portal_cam : RemoteTransform3D = $"../MainPlayer/CameraPivot/SpringArm3D/UnderworldRemoteTransfer"
-@onready var _displaying_overworld = false
-@onready var _portal_viewport : SubViewport = $"../MainPlayer/PortalViewport"
-@onready var _portal_area : Area3D = $PortalArea
-@onready var _exit_trigger_1 : Area3D = $ExitTrigger1
-@onready var _exit_trigger_2 : Area3D = $ExitTrigger2
+@onready var _portal_displaying_overworld = false
+@onready var _main_cam_in_overworld = true
+@onready var _player_in_portal : bool = false
 @onready var _on_cooldown : bool = false
-
+@onready var _portal_area : Area3D = find_child("PortalArea")
+@onready var _portal_core : Area3D = find_child("PortalCoreArea")
 @export var width : float = 5.0
 @export var height : float = 5.0
 @onready var _thickness : float = 0.2
@@ -20,70 +19,54 @@ extends MeshInstance3D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	_spawn_frame()
-		
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	## You are in overworld looking at underworld portal
-	if !_on_cooldown && _portal_area.has_overlapping_areas() && !_displaying_overworld:
-		_on_cooldown = true
-		get_tree().call_group("portals", "flip_portal")
-		_on_cooldown = true
-		var player_cam_parent = _player_overworld_cam.get_parent()
-		#_player_overworld_cam.reparent(_player_underworld_cam.get_parent())
-		_player_underworld_cam.reparent(player_cam_parent)
-		print("portalling")
-	
-	## You are in underworld looking at overworld portal
-	elif !_on_cooldown && _portal_area.has_overlapping_areas() && _displaying_overworld:
-		_on_cooldown = true
-		get_tree().call_group("portals", "flip_portal")
-		var player_cam_parent = _player_underworld_cam.get_parent()
-		#_player_underworld_cam.reparent(_player_overworld_cam.get_parent())
-		_player_overworld_cam.reparent(player_cam_parent)
-	
-	if _exit_trigger_1.has_overlapping_areas() || _exit_trigger_2.has_overlapping_areas():
-		print("resetting")
-		if _on_cooldown:
-			get_tree().call_group("portals", "flip_cameras")
+	if _portal_area.has_overlapping_areas():
+		_player_in_portal = true
+	else:
+		_player_in_portal = false
 		_on_cooldown = false
-	#var mat = mesh.surface_get_material(0)
-	##mat.set_shader_parameter("underworld_texture", portal_viewport)
-	#if !_cooldown_area.has_overlapping_bodies():
-		#_on_cooldown = false
 	## You are in overworld looking at underworld portal
-	#if !_on_cooldown && _portal_area.has_overlapping_areas() && !_displaying_overworld:
-		#get_tree().call_group("portals", "flip_portal")
-		#_on_cooldown = true
-		#var player_cam_parent = _player_overworld_cam.get_parent()
+	if !_on_cooldown && _player_in_portal:
+		print("Flipping portal cam")
+		_on_cooldown = true
+		get_tree().call_group("portals", "flip_portal")
 		#_player_overworld_cam.reparent(_player_underworld_cam.get_parent())
-		#_player_underworld_cam.reparent(player_cam_parent)
-	## You are in underworld looking at overworld portal
-	#elif !_on_cooldown && _portal_area.has_overlapping_areas() && _displaying_overworld:
-		#get_tree().call_group("portals", "flip_portal")
-		#_on_cooldown = true
-		#var player_cam_parent = _player_underworld_cam.get_parent()
-		#_player_underworld_cam.reparent(_player_overworld_cam.get_parent())
-		#_player_overworld_cam.reparent(player_cam_parent)
+		if _main_cam_in_overworld:
+			_main_cam_in_overworld = false
+			_player_overworld_cam.reparent(_portal_viewport_cam_parent)
+			_player_underworld_cam.reparent(_main_viewport_cam_parent)
+		else: 
+			_main_cam_in_overworld = true
+			_player_overworld_cam.reparent(_main_viewport_cam_parent)
+			_player_underworld_cam.reparent(_portal_viewport_cam_parent)
+	
+	# Camera flipping
+	if _portal_core.has_overlapping_areas():
+		print("portal core hit, flipping cams")
+		if _on_cooldown:
+			flip_cameras()
 
 func flip_portal() -> void:
-	if _displaying_overworld:
-		print("Flipping to overworld")
-		_displaying_overworld = true
+	if _portal_displaying_overworld:
+		_portal_displaying_overworld = false
 		set_layer_mask_value(2, false)
 		set_layer_mask_value(1, true)
-	elif !_displaying_overworld:
-		print("Flipping to underworld")
-		_displaying_overworld = false
+	elif !_portal_displaying_overworld:
+		_portal_displaying_overworld = true
 		set_layer_mask_value(2, true)
 		set_layer_mask_value(1, false)
 
 func flip_cameras() -> void:
-	if !_displaying_overworld:
-		_remote_portal_cam.set_remote_node(_player_overworld_cam.get_path())
-	elif _displaying_overworld:
-		_remote_portal_cam.set_remote_node(_player_underworld_cam.get_path())
+	# In overworld looking at underworld portal
+	if _main_cam_in_overworld:
+		_remote_portal_cam.set_remote_node(_portal_viewport_cam_parent.get_child(0).get_path())
+		_main_cam_in_overworld = false
+	# In underworld looking at overworld portal
+	elif !_main_cam_in_overworld:
+		_remote_portal_cam.set_remote_node(_portal_viewport_cam_parent.get_child(0).get_path())
+		_main_cam_in_overworld = true
 
 func _spawn_frame() -> void:
 	# Create mesh instances for frame
@@ -122,17 +105,19 @@ func _spawn_frame() -> void:
 	right_side.position = Vector3(width/2.0 + 0.1, 0.0, 0.0)
 	bottom.position = Vector3(0.0, -height/2.0 - 0.1, 0.0)
 	
-	# adjust portal collider
+	top.set_layer_mask_value(2, true)
+	left_side.set_layer_mask_value(2, true)
+	right_side.set_layer_mask_value(2, true)
+	bottom.set_layer_mask_value(2, true)
+	
+	# adjust portal area collider
 	var portal_col = _portal_area.find_child("PortalCol")
 	portal_col.shape.set_size(Vector3(width, height, _thickness/2.0))
 	print(portal_col.shape.size)
+
+	# adjust portal core area collider
+	var core_col = _portal_core.find_child("PortalCol")
+	core_col.shape.set_size(Vector3(width, height, _thickness / 3.0))
 	
-	# Init exit triggers
-	var trigger = _exit_trigger_1.find_child("PortalCol")
-	trigger.shape.set_size(Vector3(width, height, _thickness/2.0))
-	trigger = _exit_trigger_2.find_child("PortalCol")
-	trigger.shape.set_size(Vector3(width, height, _thickness/2.0))
-	_exit_trigger_1.position.z += _thickness
-	_exit_trigger_2.position.z -= _thickness
 	
 	
