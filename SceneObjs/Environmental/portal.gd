d2740b12c9e1509a6552ff678e6fa5c05e8404b7
unknown <wery231@gmail.com>
2026-08-27 11:27:14 -0700

    Camera handoff for portal

--- SceneObjs/Environmental/portal.gd
+++ SceneObjs/Environmental/portal.gd
@@ -15,6 +15,9 @@ extends MeshInstance3D
 @export var width : float = 5.0
 @export var height : float = 5.0
 @onready var _thickness : float = 0.2
+@onready var _player : CharacterBody3D = $"../MainPlayer"
+@onready var _player_entry_point : Vector3 = Vector3.ONE
+@onready var _curr_player_to_portal_norm : Vector3 = Vector3.FORWARD
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -22,31 +25,46 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
+	var player_pos_in_local = to_local(_player.position)
+	# Compare new norm to last frames norm to dermine direction
+	var new_portal_to_player_norm : Vector3 = Vector3(0.0, 0.0, player_pos_in_local.z / abs(player_pos_in_local.z))
+	var player_passed_center : bool = false
+	# norm has flipped, ie player crossed core
+	if new_portal_to_player_norm != _curr_player_to_portal_norm:
+		player_passed_center = true
+	_curr_player_to_portal_norm = new_portal_to_player_norm
+	
+	
+	
+	# Player just passsed through the core
 	if _portal_area.has_overlapping_areas():
 		_player_in_portal = true
-	else:
-		_player_in_portal = false
-		_on_cooldown = false
-	## You are in overworld looking at underworld portal
-	if !_on_cooldown && _player_in_portal:
-		print("Flipping portal cam")
-		_on_cooldown = true
-		get_tree().call_group("portals", "flip_portal")
-		#_player_overworld_cam.reparent(_player_underworld_cam.get_parent())
-		if _main_cam_in_overworld:
-			_main_cam_in_overworld = false
-			_player_overworld_cam.reparent(_portal_viewport_cam_parent)
-			_player_underworld_cam.reparent(_main_viewport_cam_parent)
-		else: 
-			_main_cam_in_overworld = true
-			_player_overworld_cam.reparent(_main_viewport_cam_parent)
-			_player_underworld_cam.reparent(_portal_viewport_cam_parent)
+		if player_passed_center:
+			print("Flipping portal cam")
+			get_tree().call_group("portals", "flip_portal")
+			#_player_overworld_cam.reparent(_player_underworld_cam.get_parent())
+			if _main_cam_in_overworld:
+				_main_cam_in_overworld = false
+				_player_overworld_cam.reparent(_portal_viewport_cam_parent)
+				_player_underworld_cam.reparent(_main_viewport_cam_parent)
+			else: 
+				_main_cam_in_overworld = true
+				_player_overworld_cam.reparent(_main_viewport_cam_parent)
+				_player_underworld_cam.reparent(_portal_viewport_cam_parent)
 	
+	# outside portal
+	if !_portal_area.has_overlapping_areas() && _player_in_portal:
+		flip_cameras()
+		_player_in_portal = false
+
+		
+
 	# Camera flipping
-	if _portal_core.has_overlapping_areas():
-		print("portal core hit, flipping cams")
-		if _on_cooldown:
-			flip_cameras()
+	#if player_in_front_of_core > 0.0:
+		#if !_on_cooldown:
+			#print("portal core hit, flipping cams")
+			#flip_cameras()
+			#_on_cooldown = true
 
 func flip_portal() -> void:
 	if _portal_displaying_overworld:
@@ -61,11 +79,11 @@ func flip_portal() -> void:
 func flip_cameras() -> void:
 	# In overworld looking at underworld portal
 	if _main_cam_in_overworld:
-		_remote_portal_cam.set_remote_node(_portal_viewport_cam_parent.get_child(0).get_path())
+		_remote_portal_cam.set_remote_node(_main_cam_in_overworld.get_path())
 		_main_cam_in_overworld = false
 	# In underworld looking at overworld portal
 	elif !_main_cam_in_overworld:
-		_remote_portal_cam.set_remote_node(_portal_viewport_cam_parent.get_child(0).get_path())
+		_remote_portal_cam.set_remote_node(_player_underworld_cam.get_child(0).get_path())
 		_main_cam_in_overworld = true
 
 func _spawn_frame() -> void:
@@ -112,12 +130,9 @@ func _spawn_frame() -> void:
 	
 	# adjust portal area collider
 	var portal_col = _portal_area.find_child("PortalCol")
-	portal_col.shape.set_size(Vector3(width, height, _thickness/2.0))
+	portal_col.shape.set_size(Vector3(width, height, _thickness))
 	print(portal_col.shape.size)
 
-	# adjust portal core area collider
-	var core_col = _portal_core.find_child("PortalCol")
-	core_col.shape.set_size(Vector3(width, height, _thickness / 3.0))
 	
 	
 	
